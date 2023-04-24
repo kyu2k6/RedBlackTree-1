fdc26d7c041a3a54669537c51fa57fb05f0c01d3
U-BSD\411915 <411915@SHS-HJ86ND2.beaverton.k12.or.us>
2023-04-24 14:20:13 -0700

    2

--- main.cpp
+++ main.cpp
@@ -4,3 +4,65 @@
 #include "node.h"
 
 using namespace std;
+
+//same function from binary search tree
+struct Trunk { //Used for printing
+    Trunk *previous;
+    char* str;
+
+    Trunk(Trunk* previous, char* str) {
+        this->previous = previous;
+        this->str = str;
+    }
+};
+
+void showTrunks(Trunk* p) { //Uesd for printing
+    if (p == NULL) 
+        return;
+
+    showTrunks(p->previous);
+
+    cout << p->str;
+}
+
+void print(Node* head, Trunk *previous, bool prev);
+
+int main() {
+	
+	
+
+
+	return 0;
+}
+
+void print(Node* head, Trunk *previous, bool prev) {
+
+	if (head == NULL) {
+		return;
+	}
+
+	char* prevStr = (char*)("    ");
+	Trunk *trunk = new Trunk(previous, prevStr);
+	print(head -> getPrev(), trunk, true);
+
+	if (!previous) {
+		trunk -> str = (char*)("---");
+	}
+	else if (prev) {
+		trunk -> str = (char*)(".---");
+		prevStr = (char*)("   |");
+	}
+	else {
+		trunk -> str = (char*)("'---");
+		previous -> str = prevStr;
+	}
+	showTrunks(trunk);
+	cout << " " << head->getData() << endl;
+
+	if (previous) {
+		previous -> str = prevStr;
+	}
+	trunk->str = (char*)("   |");
+	print(head -> getNext(), trunk, false);
+}
+

36db1683a423863f5f488810819309d68ef8a235
U-BSD\411915 <411915@SHS-HJ86ND2.beaverton.k12.or.us>
2023-05-10 13:42:21 -0700

    final

--- node.h
+++ node.h
@@ -9,11 +9,17 @@ class Node {
 		Node* getNext();
 		Node* getPrev();
 		int getData();
+		Node* getParent();
+		int getColor();
 
+		void setColor(int);
+		void setParent(Node*);
 		void setNext(Node*);
 		void setPrev(Node*);
 		void setData(int);
 	private:
+		int color;
+		Node* parent;
 		int data;
 		Node* next;
 		Node* prev;
